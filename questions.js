75996e2a040650b125f093f00bc6dc127d3f0c85
Alex Marquez <marquez798@gmail.com>
2023-05-13 17:53:00 -0500

    added function

---
+++ questions.js
@@ -0,0 +1,11 @@
+const questions = [
+    {
+      type: "input",
+        message: "what is the name of your project",
+        name: "project name",
+        default: "README GEN"  
+    }
+    
+]
+
+module.exports = questions
