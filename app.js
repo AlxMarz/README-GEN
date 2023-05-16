de51f5435802c6aa380c983560ca12a21d22ed35
Alex Marquez <marquez798@gmail.com>
2023-05-16 00:03:28 -0500

    working app

--- app.js
+++ app.js
@@ -2,10 +2,6 @@ const {questions} = require('./questions');
 const inquirer = require("inquirer");
 const generateMarkdown = require('./untilities/generateMarkdown');
 
-// const { default: Choice } = require('inquirer/lib/objects/choice');
-// const { default: Choices } = require('inquirer/lib/objects/choices');
-// const util = require('util')
-const Choices = require("inquirer/lib/objects/choices")
 
 function askQuestions(questions){
     let qNum = 0;
@@ -16,7 +12,7 @@ function askQuestions(questions){
             type: questions[qNum].type,
             name: questions[qNum].name,
             message: questions[qNum].message,
-            Choices: questions[qNum].Choices
+            choices: questions[qNum].choices
         })
         const answerName = questions[qNum].name;
         response[answerName] = answer[questions[qNum].name];
@@ -36,20 +32,3 @@ function init(){
 
 init()
 
-// function askQuestions(){
-//     inquirer.prompt(questions).then(function(answers) {
-//         console.log(answers);
-
-//     })
-
-// }
-
-// function main(){
-//     console.log(questions);
-//     askQuestions()
-
-// }
-
-// // render
-
-// main();
--- untilities/generateMarkdown.js
+++ untilities/generateMarkdown.js
@@ -1,8 +1,9 @@
 const fs = require('fs');
+
 function generateMarkdown(response){
-    if (response.liscense === "mit") {
+    if (response.license === "mit") {
         badge = "https://badgen.net/github/license/micromatch/micromatch";
-    } else if (response.liscense === "gpl") {
+    } else if (response.license === "gpl") {
         badge = "https://badgen.net/snapcraft/license/okular";
     } else {
         badge = "https://badgen.net/gitlab/license/gitlab-org/omnibus-gitlab";
@@ -40,7 +41,7 @@ ${response.credits}
     
 ![License Badge](${badge})
     
----`, err => err ? console.log("There was an error!") : console.log("README generated!"))
+---`, err => err ? console.log("There was an error!") : console.log("README has been generated!"))
 }
 
 module.exports = generateMarkdown
