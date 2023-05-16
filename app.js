33db27ef5f8d2ce6865960a27fb32722b67aca1a
Alex Marquez <marquez798@gmail.com>
2023-05-15 20:21:23 -0500

    added required

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const questions = require('./questions');
 const inquirer = require("inquirer");
 var fs = require("fs")
+const Choices = require("inquirer/lib/objects/choices")
 
 function askQuestions(){
     inquirer.prompt(questions).then(function(answers) {
